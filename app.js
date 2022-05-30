ea97db7e32603c5623df91842fad123dfefd3e63
DimitrescuTzy <rahmanradar48@gmail.com>
2022-05-31 06:16:58 +0700

    Starting node JS Toko Digital

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express")
+var app = express()
+var port = "8080"
+
+app.get("/", function(req,res){
+    return res.send("Hello-World")
+})
+
+app.listen(8080, function(){
+    console.log(`Aplikasi sedang berjalan di hhtp://127.0.0.1:${port}`)
+})
